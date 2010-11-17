940bfdf75b6625f08ad507fbd7c50b0debf08dd0
drowe67 <drowe67@01035d8c-6547-0410-b346-abe4f91aad63>
2010-11-17 03:27:10 +0000

    removed some unneeded scripts

--- src/listensim.sh
+++ src/listensim.sh
@@ -4,6 +4,6 @@
 #
 # Listen to files processed with sim.sh
 
-../script/menu.sh ../raw/$1.raw $1_uq.raw $1_phase0.raw $1_phase0_lpc10.raw $1_phase0_lpc10_dec.raw $2 $3
+../script/menu.sh ../raw/$1.raw $1_uq.raw $1_phase0.raw $1_lpc10.raw $1_phase0_lpc10.raw $1_phase0_lpc10_dec.raw $2 $3
 
 
